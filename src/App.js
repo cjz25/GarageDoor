ab5351180d9b4b26173d51b3785c9daa0ac4643f
cjz <>
2022-07-14 00:47:49 +0800

    feat: create the garage door which supports "open", "close" and "stop"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+  const DOOR_HEIGHT = 500;
+  const [height, setHeight] = useState(DOOR_HEIGHT);
+  const [isMoving, setIsMoving] = useState(false);
+  const [isUp, setIsUp] = useState(true);
+
+  const stopDoor = () => {
+    setIsMoving(false);
+  };
+
+  const openDoor = () => {
+    setIsUp(true);
+    setIsMoving(true);
+  };
+
+  const closeDoor = () => {
+    setIsUp(false);
+    setIsMoving(true);
+  };
+
+  useEffect(() => {
+    if (!isMoving) {
+      return;
+    }
+
+    if ((isUp && height === 0) || (!isUp && height === DOOR_HEIGHT)) {
+      setIsMoving(false);
+      return;
+    }
+
+    const intervalId = setInterval(() => {
+      setHeight((h) => (isUp ? h - 1 : h + 1));
+    }, 10);
+
+    return () => clearInterval(intervalId);
+  }, [isMoving, isUp, height]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="garage">
+        <div className="garageDoor" style={{ height: height }}></div>
+      </div>
+      <button onClick={openDoor}>Open ▲</button>
+      <button onClick={stopDoor}>Stop ■</button>
+      <button onClick={closeDoor}>Close ▼</button>
     </div>
   );
 }
-
-export default App;
